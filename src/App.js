8faf03bc23e95d7195b599bd0af0c7e3f3b44ccd
zedPai <weichih.pai@imersive.com>
2019-04-14 04:46:11 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator, SignOut } from 'aws-amplify-react'
+import Amplify, { Hub } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { user: null }
+    Hub.listen('auth', (data) => {
+      const { payload } = data;
+      this.loadUser(payload.data.username,
+        payload.event);
+      console.log(data)
+      // console.log('A new auth event has happened: ',
+      //   data.payload.data.username + ' has ' + data.payload.event);
+    })
+  }
+  setUser = (val) => {
+    this.setState({ user: val });
+  }
+
+  // onHubCapsule(capsule) {
+  //   this.loadUser();
+  // }
+
+  loadUser = (user, event) => {
+    console.log(user + event)
+    if ('signOut' === event) {
+      this.setState({ user: null });
+      return;
+    }
+    this.setState({ user: user })
+  }
+
   render() {
+    var user = this.state.user;
     return (
       <div className="App">
         <header className="App-header">
@@ -19,10 +53,16 @@ class App extends Component {
           >
             Learn React
           </a>
+          {/* {this.show()} */}
+          {/* {!user && <Authenticator />} */}
+          {<Authenticator />}
+          {user && <p>You are signed in as {user} </p>}
+          {user && <SignOut />}
         </header>
       </div>
     );
   }
 }
 
+//export default withAuthenticator(App, true);
 export default App;
