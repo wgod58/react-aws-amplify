7be96aff2a0f4ef53f5999f2c9767c0b4cf2a9bc
zedpai <weichih.pai@imersive.com>
2019-04-25 16:56:44 +0800

    remove firebase cdn

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+importScripts('./firebase/firebase-app.js');
+importScripts('./firebase/firebase-messaging.js');
 
 firebase.initializeApp({
   messagingSenderId: "811672300760" // 同上的拿取步驟
