3c5ce5146547afb4e3efe833cfdd0ee3806c65e9
zedpai <weichih.pai@imersive.com>
2019-04-25 16:47:04 +0800

    Finish notification save to firebase group

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
+import { API } from "aws-amplify";
 import { Button } from 'semantic-ui-react'
 import { askForPermissioToReceiveNotifications } from '../push-notification';
 
 class Home extends Component {
     constructor() {
         super();
-        this.state = { token: null };
+        this.state = { token: null, result: null };
     }
     PermissioToReceiveNotifications = async () => {
         var token = await askForPermissioToReceiveNotifications()
-        this.setState({ token })
-        return null
+        var result = await API.post('notification', '/api/noti/', { body: { id: token } });
+        this.setState({
+            "token": token,
+            result: JSON.stringify(result.success)
+        })
+        // https://n1vsy7xx99.execute-api.eu-west-1.amazonaws.com/master/api/noti/
+        return ''
     }
     // setUser = (user) => {
     //     this.setState({ user })
     // }
     render() {
-        const { token } = this.state;
+        const { result, token } = this.state;
         return (
             <div>
                 <header className="App-header">
@@ -26,6 +32,7 @@ class Home extends Component {
                     Click here to receive notifications
                 </Button>
                 <p>{token}</p>
+                <p>{result}</p>
             </div >
         )
     }
